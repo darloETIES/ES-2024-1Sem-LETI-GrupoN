f4b12f559879fbc4d43e37a509b06d0b9820e626
darloETIES <darlo@iscte-iul.pt>
2024-11-04 17:37:18 +0000

    Criação do projeto
    Criação do projeto localmente para o repositório remoto

---
+++ GestaodoTerritorio/src/main/java/grupo/n/gestaodoterritorio/GestaodoTerritorioApplication.java
@@ -0,0 +1,13 @@
+package grupo.n.gestaodoterritorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodoTerritorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodoTerritorioApplication.class, args);
+	}
+
+}
---
+++ GestaodoTerritorio/src/main/java/grupo/n/gestaodoterritorio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package grupo.n.gestaodoterritorio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestaodoTerritorioApplication.class);
+	}
+
+}
