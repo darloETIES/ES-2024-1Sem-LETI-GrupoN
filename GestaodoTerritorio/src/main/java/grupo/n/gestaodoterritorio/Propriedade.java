177ed7c4e6c4e07b8d843a969ace8cdcb0e2c4de
darloETIES <darlo@iscte-iul.pt>
2024-11-11 22:14:25 +0000

    Tarefa 2 - Ramo "tarefa2_branch1"
    Correção do ficheiro excel (linha 9325 desformatada)
    Leitura de parâmetros da propriedade com uso de WTKReader (para leitura de MULTIPOLYGON)

--- GestaodoTerritorio/src/main/java/grupo/n/gestaodoterritorio/Propriedade.java
+++ GestaodoTerritorio/src/main/java/grupo/n/gestaodoterritorio/Propriedade.java
@@ -2,7 +2,7 @@ package grupo.n.gestaodoterritorio;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
-import mil.nga.sf.geojson.MultiPolygon;
+import com.vividsolutions.jts.geom.MultiPolygon;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,14 +13,16 @@ public class Propriedade {
     private String objectId;
     private String parId;
     private String parNum;
+    private double shapeLength;
     private double shapeArea;
     private MultiPolygon geometry;
     private int owner;
 
-    public Propriedade(String objectId, String parId, String parNum, double shapeArea, MultiPolygon geometry, int owner) {
+    public Propriedade(String objectId, String parId, String parNum, double shapeLength, double shapeArea, MultiPolygon geometry, int owner) {
         this.objectId = objectId;
         this.parId = parId;
         this.parNum = parNum;
+        this.shapeLength = shapeLength;
         this.shapeArea = shapeArea;
         this.geometry = geometry;
         this.owner = owner;
